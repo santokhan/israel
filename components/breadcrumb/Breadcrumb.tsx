084b8417d59ebeb5066ea8dbfcf8104357c1eb47
Santo <santokhan1999@gmail.com>
2023-11-09 11:08:44 +0600

    Fix bug

--- components/breadcrumb/Breadcrumb.tsx
+++ components/breadcrumb/Breadcrumb.tsx
@@ -1,3 +1,5 @@
+'use client'
+
 import { usePathname } from "next/navigation"
 import { NavList } from "../header/Navbar"
 
@@ -17,11 +19,15 @@ export default function BreadCrumb() {
         return name || "";
     }
 
-    return (
-        <section className="bg-app-green">
-            <div className="w-full max-w-screen-xl mx-auto px-4 py-20 grid place-items-center">
-                <h1 className="text-4xl font-bold text-app-cream">{getName()}</h1>
-            </div>
-        </section>
-    )
+    if (path) {
+        return (
+            <section className="bg-app-green">
+                <div className="w-full max-w-screen-xl mx-auto px-4 py-20 grid place-items-center">
+                    <h1 className="text-4xl font-bold text-app-cream">{getName()}</h1>
+                </div>
+            </section>
+        )
+    } else {
+        return <></>
+    }
 }
