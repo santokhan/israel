61291888d75fda6b2e59c1bd63f6bd51e1e7be3a
Santo <santokhan1999@gmail.com>
2023-11-12 18:33:53 +0600

    Add: article page

---
+++ app/article/page.tsx
@@ -0,0 +1,7 @@
+import Article from '../../sections/article/Article'
+
+export default function Page() {
+    return <div className="max-w-screen-lg mx-auto px-4">
+        <Article></Article>
+    </div>
+}
