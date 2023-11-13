a4ca462e26a576d08f5ddc7d1a0483c600f929a5
Santo <santokhan1999@gmail.com>
2023-11-13 17:09:20 +0600

    update

---
+++ out/_next/static/chunks/app/blog/page-333e653392d1b01e.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[404],{7113:function(e,n,s){Promise.resolve().then(s.bind(s,2699)),Promise.resolve().then(s.t.bind(s,8326,23))}},function(e){e.O(0,[122,699,971,472,744],function(){return e(e.s=7113)}),_N_E=e.O()}]);
